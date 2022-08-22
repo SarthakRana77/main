b5fac0a305920def86cf20e61ff43bb1bf6cb0f7
SarthakRana77 <sarthak.rana7895@gmail.com>
2022-08-22 14:51:39 +0530

    test 2

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require('http');
+const path = require('path');
 
-const routes= require('./routes');
-console.log(routes.someText);
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server= http.createServer(routes.handler);
-server.listen(3000); 
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found',formsCss:true,productCss:true });
+});
+
+app.listen(3000);
